1cfe8fe45b9217a1f08392caf9dcdbabee1baca0
Bekcpear <ink2.0plus@gmail.com>
2017-05-31 15:34:00 +0800

    initial theme

---
+++ README.md
@@ -0,0 +1,74 @@
+# UMECloudTheme - A theme for NextCloud 12.0.0
+
+This is a theme for NextCloud, it's compatible with version 12.0.0
+
+**It's easy to install**
+
+1st. change to your NextCloud theme directory:
+
+```
+$ cd /path/to/nextcloud/themes/
+```
+
+2nd. clone this repository to the current path (also need the `git` command):
+
+```
+$ git clone --branch master https://github.com/Bekcpear/UMECloudTheme.git
+```
+
+3rd. edit `/path/to/nextcloud/themes/UMEThemeCloud/defaults.php` to do some personal settings.
+
+4th. copy your own logo and favicon images to override the corresponding images in `/path/to/nextcloud/themes/UMEThemeCloud/core/img` directory.
+
+5th. fix the permission of UMECloudTheme directory and all it's subdirectories and files.
+
+6th. use the following function to replace `adjustControlsWidth` function in the `/path/to/nextcloud/core/js/js.js` file (the codes probably between lines 1616-1642):
+
+```
+var adjustControlsWidth = function() {
+  if($('#controls').length) {
+    var controlsWidth;
+    // if there is a scrollbar …
+    if($('#app-content').get(0).scrollHeight > $('#app-content').height()) {
+      if($(window).width() > 768) {
+        controlsWidth = $('#content').width() - $('#app-navigation').width() - getScrollBarWidth();
+      } else {
+        controlsWidth = $('#content').width() - getScrollBarWidth();
+      }
+    } else { // if there is none
+      if($(window).width() > 768) {
+        controlsWidth = $('#content').width() - $('#app-navigation').width();
+      } else {
+        controlsWidth = $('#content').width();
+      }
+    }
+
+    if($(window).width() > 768
+      && ( $('#app-sidebar').hasClass('hidden')
+      || $('#app-sidebar').hasClass('disappear'))) {
+      controlsWidth -= 20;                                                                                         
+      $('#app-content').css('margin-right', 20); 
+      $('#filestable thead').css('width', controlsWidth);
+    }
+
+    $('#controls').css('width', controlsWidth);
+    $('#controls').css('min-width', controlsWidth);
+  }    
+};
+```
+
+7th. edit `/path/to/nextcloud/config/config.php` to set `'theme' => 'UMEThemeCloud',` , and save it.
+
+8th. refresh you NextCloud page (clean the cache at this time) an enjoy it.
+
+## Screenshots
+
+![](screenshots/login.png)
+
+![](screenshots/apps-files.png)
+
+![](screenshots/settings-admin.png)
+
+## LICENSE
+
+AGPL
---
+++ defaults.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * @author Björn Schießle <schiessle@owncloud.com>
+ * @author Jan-Christoph Borchardt, http://jancborchardt.net
+ * @copyright Copyright (c) 2016, ownCloud, Inc.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+class OC_Theme {
+
+	/**
+	 * Returns the base URL
+	 * @return string URL
+	 */
+	public function getBaseUrl() {
+		return 'https://c.ume.ink';
+	}
+
+	/**
+	 * Returns the URL where the sync clients are listed
+	 * @return string URL
+	 */
+	public function getSyncClientUrl() {
+		return 'https://nextcloud.com/install/#install-clients';
+	}
+
+	/**
+	 * Returns the URL to the App Store for the iOS Client
+	 * @return string URL
+	 */
+	public function getiOSClientUrl() {
+		return 'https://itunes.apple.com/us/app/nextcloud/id1125420102?mt=8';
+	}
+
+	/**
+	 * Returns the AppId for the App Store for the iOS Client
+	 * @return string AppId
+	 */
+	public function getiTunesAppId() {
+		return '1125420102';
+	}
+
+	/**
+	 * Returns the URL to Google Play for the Android Client
+	 * @return string URL
+	 */
+	public function getAndroidClientUrl() {
+		return 'https://play.google.com/store/apps/details?id=com.nextcloud.client';
+	}
+
+	/**
+	 * Returns the documentation URL
+	 * @return string URL
+	 */
+	public function getDocBaseUrl() {
+		return 'https://docs.nextcloud.com';
+	}
+
+	/**
+	 * Returns the title
+	 * @return string title
+	 */
+	public function getTitle() {
+		return 'UME Cloud';
+	}
+
+	/**
+	 * Returns the short name of the software
+	 * @return string title
+	 */
+	public function getName() {
+		return 'UME Cloud';
+	}
+
+	/**
+	 * Returns the short name of the software containing HTML strings
+	 * @return string title
+	 */
+	public function getHTMLName() {
+		return 'UME Cloud';
+	}
+
+	/**
+	 * Returns entity (e.g. company name) - used for footer, copyright
+	 * @return string entity name
+	 */
+	public function getEntity() {
+		return 'UME Cloud';
+	}
+
+	/**
+	 * Returns slogan
+	 * @return string slogan
+	 */
+	public function getSlogan() {
+		return 'For private use.';
+	}
+
+	/**
+	 * Returns logo claim
+	 * @return string logo claim
+	 */
+	public function getLogoClaim() {
+		return '';
+	}
+
+	/**
+	 * Returns short version of the footer
+	 * @return string short footer
+	 */
+	public function getShortFooter() {
+		$footer = '<a href="'.$this->getBaseUrl().'" target="_blank\">'.$this->getEntity().'</a>'. ' - ' . $this->getSlogan();
+
+		return $footer;
+	}
+
+	/**
+	 * Returns long version of the footer
+	 * @return string long footer
+	 */
+	public function getLongFooter() {
+		$footer = '<a href="'.$this->getBaseUrl().'" target="_blank\">'.$this->getEntity().'</a>'. ' - ' . $this->getSlogan();
+
+		return $footer;
+	}
+
+	public function buildDocLinkToKey($key) {
+		return $this->getDocBaseUrl() . '/server/12/go.php?to=' . $key;
+	}
+
+
+	/**
+	 * Returns mail header color
+	 * @return string
+	 */
+	public function getColorPrimary() {
+		return '#7986CB';
+	}
+
+	/**
+	 * Returns variables to overload defaults from core/css/variables.scss
+	 * @return array
+	 */
+	public function getScssVariables() {
+		return [
+			'color-primary' => '#7986CB'
+		];
+	}
+
+}
